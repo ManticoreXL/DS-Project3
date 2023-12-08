ad11f36e71952128de0476fb67dff3a7f8eff79a
ManticoreXL <zip3366@naver.com>
2023-12-08 22:40:58 +0900

    commit

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -5,10 +5,19 @@
 // constructor of ListGraph
 ListGraph::ListGraph(bool type, int size) : Graph(type, size)
 {
+    // create new m_List and kw_graph
+    m_List = new map<int, int>[size];
+    kw_graph = new vector<int>;
 }
 
+// destructor of ListGraph
 ListGraph::~ListGraph()
 {
+    // delete memory of m_List and kw_graph
+    if (m_List)
+        delete[] m_List;
+    if (kw_graph)
+        delete[] kw_graph;
 }
 
 // get undirected graph list
@@ -22,6 +31,7 @@ void ListGraph::getAdjacentEdges(int vertex, map<int, int> *m) // Definition of 
 
     if (m_Type == true) // case: directed -> undirected
     {
+        // todo
     }
     else // case 2: undirected -> undirected
     {
@@ -56,14 +66,45 @@ void ListGraph::getAdjacentEdgesDirect(int vertex, map<int, int> *m) // Definiti
     {
         for (int i = 0; i < m_Size; i++)
         {
+            // todo
         }
     }
 }
 
 void ListGraph::insertEdge(int from, int to, int weight) // Definition of insertEdge
 {
+    if (m_List == nullptr)
+    {
+        throw "void ListGraph::insertEdge(int from, int to, int weight) - list is empty.";
+        return;
+    }
+
+    // insert (from, to)->weight into map
+    m_List[from - 1].insert(pair<int, int>(to, weight));
 }
 
 bool ListGraph::printGraph(ofstream *fout) // Definition of print Graph
 {
+    if (m_List == nullptr)
+    {
+        throw "bool ListGraph::printGraph(ofstream *fout) - list is empty.";
+        return false;
+    }
+
+    for (int i = 0; i < m_Size; i++)
+    {
+        // print from vertex
+        *fout << i << "-> " << endl;
+
+        // print adjacency list's item
+        for (auto it = m_List[i].begin(); it != m_List[i].end(); it++)
+        {
+            *fout << "(" << it->first << ", " << it->second << ")";
+            if (++it != m_List[i].end()) // check for ->
+                *fout << " -> ";
+
+            it--; // move back
+            *fout << endl;
+        }
+    }
 }
